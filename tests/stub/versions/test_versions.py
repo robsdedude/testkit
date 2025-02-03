bf9e4d1823ab970f9d401b5dba512d56c9859cc7
Robsdedude <dev@rouvenbauer.de>
2025-02-03 20:38:03 +0100

    Add Rust specific skip

--- tests/stub/versions/test_versions.py
+++ tests/stub/versions/test_versions.py
@@ -340,7 +340,7 @@ class TestProtocolVersions(TestkitTestCase):
     @driver_feature(types.Feature.BOLT_5_7)
     def test_should_reject_server_using_verify_connectivity_bolt_5x7(self):
         # TODO remove this block once fixed
-        if get_driver_name() in ["dotnet", "go", "javascript"]:
+        if get_driver_name() in ["dotnet", "go", "javascript", "rust"]:
             self.skipTest("Driver does not check server agent string")
         self._test_should_reject_server_using_verify_connectivity(
             version="5.7", script="v5x1_and_up_optional_hello.script"
